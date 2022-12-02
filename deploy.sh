31c1535936270734940dceb1a8626fe62e2d8ef5
Jaiganesh Girinathan <ganeshji@amazon.com>
2022-12-02 17:53:19 -0500

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export AWS_ACCESS_KEY_ID=AKIAZ2EEXQUVT2OTAILU
-export AWS_SECRET_ACCESS_KEY=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
+# export AWS_ACCESS_KEY_ID=
+# export AWS_SECRET_ACCESS_KEY=
 # npm install
 # cdktf provider add "aws@~>4.0" random
 cdktf deploy
